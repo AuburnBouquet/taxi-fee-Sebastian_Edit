78b42cb8be3cfd7b185434700556b992909c1f06
Sebastian Kim <sebastian_jr@yeah.net>
2018-04-12 23:15:54 +0800

    taxi-fee completed

--- main/main.js
+++ main/main.js
@@ -1,4 +1,8 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(distance, wait_time) {
+    let init;
+    if(distance < 8) init = [6, (distance - 2) * 0.8];
+    else init = [11, (distance - 8) * 1.2];
+    if(wait_time) wait_time = 0.25 * wait_time;
+    if(init[1] < 0) return ('总计：'+Math.round(6+wait_time)+' 元');
+    else return ('总计：'+Math.round(init[0]+init[1]+wait_time)+' 元');
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,32 @@
-const main = require('../main/main');
+const main = require('../main/main.js');
 
 describe('taxi fee', function () {
-    // write your tests here...
+
+    it('x < 2km', function () {
+        let expected_costs = '总计：6 元';
+        let result = main(1, 0);
+
+        expect(result).toEqual(expected_costs);
+    });
+
+    it('2km<= x < 8km', function () {
+        let expected_costs = '总计：9 元';
+        let result = main(6, 0);
+
+        expect(result).toEqual(expected_costs);
+    });
+
+    it('x >= 8km', function () {
+        let expected_costs = '总计：15 元';
+        let result = main(11.1, 0);
+
+        expect(result).toEqual(expected_costs);
+    });
+
+    it('x >= 8km and wait 3 mins', function () {
+        let expected_costs = '总计：15 元';
+        let result = main(11.1, 3);
+
+        expect(result).toEqual(expected_costs);
+    });
 });
